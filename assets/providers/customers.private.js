7addd340cc9acd9edfad60893cedc5232508d46d
michaelbarney <michaelbarneyjunior@gmail.com>
2022-11-29 10:37:08 -0300

    Initial Commit

---
+++ assets/providers/customers.private.js
@@ -0,0 +1,259 @@
+// Create global variable to memoize customer data
+// so that we do not ping airtable for all customers every page load
+
+let lastFetch = "";
+let customers = [];
+
+const initAirtable = (context) => {
+  const Airtable = require("airtable");
+  return new Airtable({ apiKey: context.AIRTABLE_API_KEY }).base(
+    context.AIRTABLE_BASE_ID
+  );
+};
+
+const setLastFetch = () => {
+  const date = new Date();
+  lastFetch = date.toISOString();
+};
+
+const getAirtableCustomerByParams = async (context, params) => {
+  const base = initAirtable(context);
+
+  return new Promise((resolve, reject) => {
+    let customer;
+
+    base("Candidates")
+      .select(params)
+      .eachPage(
+        function page(records, fetchNextPage) {
+          customer = formatCustomerRecord(records[0]);
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            reject(JSON.stringify(err));
+          }
+
+          resolve(customer);
+        }
+      );
+  });
+};
+
+// Retrieve customers from Airtable
+const getAllAirtableCustomers = async (context, workerId) => {
+  const base = initAirtable(context);
+
+  const querySettings = {
+    view: "Grid view",
+    pageSize: 100,
+  };
+
+  // Filter airtable results by owner if we know
+  // the worker ID
+  if (workerId) {
+    querySettings.filterByFormula = `{Owner} = '${workerId}'`;
+  }
+
+  return new Promise((resolve, reject) => {
+    const formattedCustomers = [];
+
+    base("Candidates")
+      .select(querySettings)
+      .eachPage(
+        function page(records, fetchNextPage) {
+          // This function (`page`) will get called for each page of records.
+
+          records.forEach(function (record) {
+            const formattedRecord = formatCustomerRecord(record);
+            formattedCustomers.push(formattedRecord);
+          });
+
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            reject(err);
+          }
+          resolve(formattedCustomers);
+        }
+      );
+  });
+};
+
+const getNewCustomers = (context, worker) => {
+  const base = initAirtable(context);
+
+  const querySettings = {
+    view: "Grid view",
+    pageSize: 100,
+    filterByFormula: `IS_AFTER(CREATED_TIME(), DATETIME_PARSE('${lastFetch}'))`,
+  };
+
+  return new Promise((resolve, reject) => {
+    const newCustomers = [];
+
+    base("Candidates")
+      .select(querySettings)
+      .eachPage(
+        function page(records, fetchNextPage) {
+          // This function (`page`) will get called for each page of records.
+
+          records.forEach(function (record) {
+            const formattedRecord = formatCustomerRecord(record);
+            newCustomers.push(formattedRecord);
+          });
+
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            reject(err);
+          }
+          resolve(newCustomers);
+        }
+      );
+  });
+};
+
+const formatCustomerRecord = (customerRecord) => {
+  try {
+    const unformattedAddress = customerRecord.get("Contact");
+    const formattedAddress = unformattedAddress.replace(/[-()]/gm, "");
+
+    return {
+      customer_id: `${customerRecord.get("id")}`,
+      display_name: `${customerRecord.get("Candidate")}`,
+      position: `${customerRecord.get("Position")}`,
+      channels: [
+        { type: "sms", value: `${customerRecord.get("Contact")}` },
+        {
+          type: "whatsapp",
+          value: `whatsapp:${customerRecord.get("Contact")}`,
+        },
+      ],
+      links: [
+        {
+          type: "LinkedIn",
+          value: `${customerRecord.get("LinkedIn")}`,
+          display_name: "LinkedIn",
+        },
+        {
+          type: "Email",
+          value: `mailto:${customerRecord.get("Email")}`,
+          display_name: "Email Address",
+        },
+      ],
+      details: {
+        title: "Information",
+        content: `${customerRecord.get("Notes")}`,
+      },
+      worker: `${customerRecord.get("Owner")}`,
+      address: `${formattedAddress}`,
+    };
+  } catch (err) {
+    return new Error(err);
+  }
+};
+
+const findWorkerForCustomer = async (context, customerNumber) => {
+  if (customers.length === 0) {
+    customers = await getAllAirtableCustomers(context);
+  }
+
+  const workerForCustomer = customers.filter((customer) => {
+    if (customerNumber.includes(customer.address)) {
+      return customer;
+    }
+
+    return null;
+  });
+
+  if (workerForCustomer.length > 0) {
+    return workerForCustomer[0].worker;
+  }
+
+  return null;
+};
+
+const findRandomWorker = async (context) => {
+  if (customers.length === 0) {
+    customers = await getAllAirtableCustomers(context);
+  }
+
+  const uniqueWorkers = [];
+
+  for (const customer of customers) {
+    if (!uniqueWorkers.includes(customer.worker)) {
+      uniqueWorkers.push(customer.worker);
+    }
+  }
+
+  const randomIndex = Math.floor(Math.random() * uniqueWorkers.length);
+  return uniqueWorkers[randomIndex];
+};
+
+const getCustomersList = async (context, worker, pageSize, anchor) => {
+  // Pull airtable customers on first load,
+  // otherwise use what's stored in memory
+  if (anchor === undefined || customers.length === 0) {
+    customers = await getAllAirtableCustomers(context, worker);
+    setLastFetch();
+  } else {
+    const newCustomers = await getNewCustomers(context, worker);
+    customers = customers.concat(newCustomers);
+    setLastFetch();
+  }
+
+  console.log("Test 2: ", worker, anchor, customers.length);
+  console.log("Customers: ", customers);
+
+  const workerCustomers = customers.filter(
+    (customer) => customer.worker === worker
+  );
+  const list = workerCustomers.map((customer) => ({
+    display_name: customer.display_name,
+    customer_id: customer.customer_id,
+    avatar: customer.avatar,
+  }));
+
+  if (!pageSize) {
+    return list;
+  }
+
+  if (anchor) {
+    const lastIndex = list.findIndex(
+      (c) => String(c.customer_id) === String(anchor)
+    );
+    const nextIndex = lastIndex + 1;
+    return list.slice(nextIndex, nextIndex + pageSize);
+  } else {
+    return list.slice(0, pageSize);
+  }
+};
+
+const getCustomerByNumber = async (context, customerNumber) => {
+  const customer = await getAirtableCustomerByParams(context, {
+    view: "Grid view",
+    filterByFormula: `{Contact} = '${customerNumber}'`,
+    maxRecords: 1,
+  });
+  return customer;
+};
+
+const getCustomerById = async (context, customerId) => {
+  const customer = await getAirtableCustomerByParams(context, {
+    view: "Grid view",
+    filterByFormula: `{id} = '${customerId}'`,
+    maxRecords: 1,
+  });
+  return customer;
+};
+
+module.exports = {
+  findWorkerForCustomer,
+  findRandomWorker,
+  getCustomerById,
+  getCustomersList,
+  getCustomerByNumber,
+};
---
+++ callbacks/routing.protected.js
@@ -0,0 +1,79 @@
+// eslint-disable-next-line no-undef
+const path = Runtime.getAssets()["/providers/customers.js"].path;
+const { findWorkerForCustomer, findRandomWorker } = require(path);
+
+exports.handler = async function (context, event, callback) {
+  console.log("Handling custom routing callback.");
+
+  const client = context.getTwilioClient();
+
+  try {
+    const conversationSid = event.ConversationSid;
+    const customerNumber = event["MessagingBinding.Address"].replace(
+      "whatsapp:",
+      ""
+    );
+
+    const workerIdentity = await routeConversation(
+      context,
+      conversationSid,
+      customerNumber
+    );
+    const resp = await routeConversationToWorker(
+      client,
+      conversationSid,
+      workerIdentity
+    );
+
+    // Remove chatbot from the conversation
+    console.log("DBG SID: ", event.WebhookSid);
+    if (event.WebhookSid) {
+      console.log("DBG: Removing chatbot from the conversation.");
+      await client.conversations
+        .conversations(conversationSid)
+        .webhooks(event.WebhookSid)
+        .remove();
+    }
+
+    callback(null, resp);
+  } catch (err) {
+    callback(err);
+  }
+};
+
+const routeConversation = async (context, conversationSid, customerNumber) => {
+  let workerIdentity = await findWorkerForCustomer(context, customerNumber);
+
+  if (!workerIdentity) {
+    // customer doesn't have a worker
+    // Select a random worker
+    console.log("No assigned worker found, selecting a random worker.");
+    workerIdentity = await findRandomWorker(context);
+
+    // Or you can define default worker for unknown customers.
+    // workerIdentity = 'john@example.com'
+
+    if (!workerIdentity) {
+      throw new Error(
+        `Routing failed, please add workers to customersToWorkersMap or define a default worker. Conversation SID: ${conversationSid}`
+      );
+    }
+  }
+
+  return workerIdentity;
+};
+
+const routeConversationToWorker = async (
+  client,
+  conversationSid,
+  workerIdentity
+) => {
+  // Add worker to the conversation with a customer
+  await client.conversations
+    .conversations(conversationSid)
+    .participants.create({ identity: workerIdentity })
+    .then((participant) =>
+      console.log("Created agent participant: ", participant.sid)
+    )
+    .catch((err) => console.log(`Failed to create agent participant: ${err}`));
+};
